2b88aa608a7a549191b7106c1b38ca342f424b61
Carlos Quesada <carlos.quesada@newelevation.com>
2019-11-22 11:43:13 -0500

    initial commit of a sample component

---
+++ src/index.svelte
@@ -0,0 +1,21 @@
+<svg
+  version="1.1"
+  id="Layer_1"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  x="0px"
+  y="0px"
+  width="95.253px"
+  height="95.253px"
+  viewBox="0 0 95.253 95.253"
+  enable-background="new 0 0 95.253 95.253"
+  xml:space="preserve">
+  <circle
+    fill="#FFFFFF"
+    stroke="#000000"
+    stroke-miterlimit="10"
+    cx="47.626"
+    cy="47.626"
+    r="47.126" />
+  <circle cx="29.235" cy="36.132" r="18.391" />
+</svg>
